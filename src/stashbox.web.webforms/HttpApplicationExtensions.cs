af140215562ea03b3d7cafdd02e10e4300a4d0c6
Murray <thomas@vinylfetish.net>
2019-03-29 19:59:11 -0500

    rough pass implementation. by no means ready to use by anyone.

---
+++ src/stashbox.web.webforms/HttpApplicationExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web;
+
+namespace Stashbox.Web.WebForms
+{
+    public static class HttpApplicationExtensions
+    {
+        public static IStashboxContainer AddStashbox(this HttpApplication application)
+        {
+            if (application == null)
+            {
+                throw new ArgumentNullException(nameof(application));
+            }
+
+            return StashboxAdapter.AddStashbox();
+        }
+
+        public static IStashboxContainer GetStashboxContainer(this HttpApplication application)
+        {
+            return StashboxAdapter.GetContainer();
+        }
+    }
+}
---
+++ src/stashbox.web.webforms/StashboxAdapter.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Web;
+
+namespace Stashbox.Web.WebForms
+{
+    public static class StashboxAdapter
+    {
+        private static readonly object Lock = new object();
+
+        public static IStashboxContainer AddStashbox()
+        {
+            lock (Lock)
+            {
+                HttpRuntime.WebObjectActivator = new ContainerServiceProvider(HttpRuntime.WebObjectActivator);
+
+                return GetContainer();
+            }
+        }
+
+        [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+        public static IStashboxContainer GetContainer()
+        {
+            return (HttpRuntime.WebObjectActivator as ContainerServiceProvider)?.Container;
+        }
+    }
+}
